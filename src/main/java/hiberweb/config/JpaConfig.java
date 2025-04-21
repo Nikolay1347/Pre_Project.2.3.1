a6544bb6d8fa6bc65afbae8c1ef0b20ef996dfa5
Nikolay1347 <kolyan29041985@gmail.com>
2025-04-21 10:27:26 +0300

    v1.3

--- src/main/java/hiberweb/config/JpaConfig.java
+++ src/main/java/hiberweb/config/JpaConfig.java
@@ -1,6 +1,6 @@
 package hiberweb.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/hiberweb/controller/UsersController.java
+++ src/main/java/hiberweb/controller/UsersController.java
@@ -4,13 +4,10 @@ import hiberweb.model.User;
 import hiberweb.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Controller
